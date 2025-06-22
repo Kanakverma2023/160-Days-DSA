1384e483c3106166c059b23fae083266ee2e8275
Kanak Verma <153801680+Kanakverma2023@users.noreply.github.com>
2025-06-22 22:09:03 +0530

    Create MajorElementII.java

---
+++ Arrays/MajorElementII.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Solution {
+
+    public List<Integer> majorityElement(int[] nums) {
+        int count1 = 0, count2 = 0;
+        Integer candidate1 = null, candidate2 = null;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (candidate1 != null && num == candidate1) {
+                count1++;
+            } else if (candidate2 != null && num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {1, 1, 1, 3, 3, 2, 2, 2};
+        System.out.println("Majority elements: " + sol.majorityElement(nums));
+    }
+}
+
